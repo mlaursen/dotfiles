75ad376ce7eca605b7d6acd6268b755b163624bd
Mikkel Laursen <mlaursen03@gmail.com>
2024-03-07 22:30:27 -0500

    chore(nvim): remove old nvim directory

--- nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
-
-map("n", "<F9>", ":cp<cr>")
-map("n", "<F10>", ":cn<cr>")
-map("n", "<leader>sf", ":setlocal tw=80<cr>")
