94dd0fe8dbe05ad9975cbd0305bfd4561abe03b8
Mikkel Laursen <mlaursen03@gmail.com>
2024-05-01 14:34:53 -0400

    chore(nvim): enable copilot.

--- lazyvim/nvim/lua/plugins/coding.lua
+++ lazyvim/nvim/lua/plugins/coding.lua
@@ -133,4 +133,18 @@ return {
       { "<space>cg", "<cmd>Neogen<cr>", desc = "Generate Docs" },
     },
   },
+
+  {
+    "zbirenbaum/copilot.lua",
+    -- enabled = false,
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+      },
+      -- copilot_node_command = "node-20",
+    },
+  },
 }
