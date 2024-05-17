172eeec0dae9e8f966cb2d15c260f2f2709bab60
Mikkel Laursen <mlaursen03@gmail.com>
2024-05-17 09:41:59 -0400

    feat(nvim): update for nvim 0.10.0

--- lazyvim/nvim/lua/plugins/disabled.lua
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -8,14 +8,14 @@ return {
   -- what's what.
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 
+  -- I use luasnips
+  { "garymjr/nvim-snippets", enabled = false },
   -- I prefer my own snippets
   { "rafamadriz/friendly-snippets", enabled = false },
 
   -- Using nightfox with the colorblind mode
   -- { "folke/tokyonight.nvim", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
-
-  { "dstein64/vim-startuptime", enabled = false },
 
   -- I prefer only using "lukas-reineke/indent-blankline.nvim" since the
   -- indentation never really matters for me and this plugin makes it even
@@ -31,7 +31,6 @@ return {
     --   },
     -- },
   },
-  { "echasnovski/mini.surround", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
 
