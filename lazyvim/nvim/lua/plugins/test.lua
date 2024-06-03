7ff7abf2d631871c510f9e6ef5293dfcb325ab72
Mikkel Laursen <mlaursen03@gmail.com>
2024-06-03 11:14:14 -0400

    feat(nvim): enable dap and neotest

---
+++ lazyvim/nvim/lua/plugins/test.lua
@@ -0,0 +1,13 @@
+return {
+  { "nvim-neotest/neotest-jest" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-jest"] = {
+          args = {},
+        },
+      },
+    },
+  },
+}
