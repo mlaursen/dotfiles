260e9b25cda934270ae7f7cc13bde0737d22c228
Mikkel Laursen <mlaursen03@gmail.com>
2024-06-24 09:15:16 -0400

    fix(nvim): re-enabled mini.pairs
    I got this mixed up with surround

--- lazyvim/nvim/lua/plugins/disabled.lua
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -13,8 +13,6 @@ return {
   -- I prefer my own snippets
   { "rafamadriz/friendly-snippets", enabled = false },
 
-  { "echasnovski/mini.pairs", enabled = false },
-
   -- Using nightfox with the colorblind mode
   -- { "folke/tokyonight.nvim", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
