ee0e2378bc0d18a520367b4c24c7f54b19f76389
Mikkel Laursen <mlaursen03@gmail.com>
2024-11-09 16:44:32 -0500

    chore(nvim): update deps and spellfile

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -8,9 +8,9 @@ del({ "i", "x", "n", "s" }, "<C-s>")
 
 -- remove lazygit mappings that don't have another mapping for with vim-fugitive/fzf-lua
 -- del("n", "<leader>gg")
-del("n", "<leader>gG")
+-- del("n", "<leader>gG")
 -- del("n", "<leader>gb")
 -- del("n", "<leader>gB")
-del("n", "<leader>gf")
+-- del("n", "<leader>gf")
 -- del("n", "<leader>gl")
-del("n", "<leader>gL")
+-- del("n", "<leader>gL")
