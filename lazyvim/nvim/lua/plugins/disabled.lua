43bf81a18dce3b2c1e4e4cf325cb73d701b3347e
Mikkel Laursen <mlaursen03@gmail.com>
2024-12-01 16:25:34 -0500

    chore(nvim): update to latest deps

--- lazyvim/nvim/lua/plugins/disabled.lua
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -1,21 +1,12 @@
 return {
-  -- this adds the stupid dashboard when opening nvim with no files selected
-  { "nvimdev/dashboard-nvim", enabled = false },
-
-  -- this uses sticky positioning at the top of the screen for the current
-  -- scope. i.e. the current function or object
-  -- when I have multiple buffers visible at once, it's too hard to track
-  -- what's what.
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-
   -- I use luasnips
   { "garymjr/nvim-snippets", enabled = false },
   -- I prefer my own snippets
   { "rafamadriz/friendly-snippets", enabled = false },
 
   -- Using nightfox with the colorblind mode
-  -- { "folke/tokyonight.nvim", enabled = false },
-  -- { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 
   -- I prefer only using "lukas-reineke/indent-blankline.nvim" since the
   -- indentation never really matters for me and this plugin makes it even
@@ -25,8 +16,4 @@ return {
   { "lewis6991/gitsigns.nvim", enabled = false },
 
   { "echasnovski/mini.pairs", enabled = false },
-
-  -- this makes markdown too difficult to read as it switches between
-  -- 'conceallevel' as you type and navigate through the file
-  { "lukas-reineke/headlines.nvim", enabled = false },
 }
