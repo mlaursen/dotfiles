a159b27247f1f374989915259ad83c3fdc008485
Mikkel Laursen <mlaursen03@gmail.com>
2025-02-09 15:54:54 -0500

    chore(nvim): update lazy.lua with latest from starter

--- lazyvim/nvim/lua/config/lazy.lua
+++ lazyvim/nvim/lua/config/lazy.lua
@@ -1,16 +1,18 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
@@ -20,6 +22,7 @@ require("lazy").setup({
       import = "lazyvim.plugins",
       opts = { colorscheme = "nightfox" },
     },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
@@ -32,8 +35,13 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "nightfox" } },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
   -- I want to manually check for updates instead of seeing the popups each time I open nvim
-  checker = { enabled = false }, -- automatically check for plugin updates
+  checker = { enabled = false },
+  -- checker = {
+  --   enabled = true, -- check for plugin updates periodically
+  --   notify = false, -- notify on update
+  -- }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
