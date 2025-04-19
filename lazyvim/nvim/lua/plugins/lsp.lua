03194b0d0a64982d4085ea67a62ed663434a10dc
Mikkel Laursen <mlaursen03@gmail.com>
2025-04-19 14:12:28 -0400

    fix(nvim): apply eslint format fix

--- lazyvim/nvim/lua/plugins/lsp.lua
+++ lazyvim/nvim/lua/plugins/lsp.lua
@@ -180,14 +180,63 @@ return {
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      -- setup = {
+      --   -- example to setup with typescript.nvim
+      --   -- tsserver = function(_, opts)
+      --   --   require("typescript").setup({ server = opts })
+      --   --   return true
+      --   -- end,
+      --   -- Specify * to use this function as a fallback for any server
+      --   -- ["*"] = function(server, opts) end,
+      -- },
+
+      -- a copy of LazyVim's setup function with one change (marked inline) to fix
+      -- auto-fixing on neovim 0.11.
       setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
+        eslint = function()
+          local function get_client(buf)
+            return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
+          end
+          local formatter = LazyVim.lsp.formatter({
+            name = "eslint: lsp",
+            primary = false,
+            priority = 200,
+            filter = "eslint",
+          })
+
+          -- Use EslintFixAll on Neovim < 0.10.0
+          -- Changed from upstream: check the version explicitly instead of
+          -- looking for `vim.lsp._require`. Seems like that check stopped working
+          -- with Neovim 0.11.
+          if vim.fn.has("nvim-0.10") == 0 then
+            formatter.name = "eslint: EslintFixAll"
+            formatter.sources = function(buf)
+              local client = get_client(buf)
+              return client and { "eslint" } or {}
+            end
+
+            formatter.format = function(buf)
+              local client = get_client(buf)
+              if client then
+                local pull_diagnostics =
+                  vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id, false) })
+
+                -- Older versions of the ESLint language server send push
+                -- diagnostics rather than using pull. We support both for
+                -- backwards compatibility.
+                local push_diagnostics =
+                  vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id, true) })
+
+                if (#pull_diagnostics + #push_diagnostics) > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end
+          end
+
+          -- register the formatter with LazyVim
+          LazyVim.format.register(formatter)
+        end,
       },
     },
 
