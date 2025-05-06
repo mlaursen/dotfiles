df93ba47eaad9f5a6a95ace47d58d4471b9e1491
Mikkel Laursen <mlaursen03@gmail.com>
2025-05-06 15:02:56 -0400

    feat(neotest): switch to my forks of jest/vitest to update snapshots

--- lazyvim/nvim/lua/plugins/test.lua
+++ lazyvim/nvim/lua/plugins/test.lua
@@ -2,75 +2,38 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
-      "nvim-neotest/neotest-jest",
-      "marilari88/neotest-vitest",
+      { "nvim-neotest/neotest-jest", url = "https://github.com/mlaursen/neotest-jest" },
+      { "marilari88/neotest-vitest", url = "https://github.com/mlaursen/neotest-vitest" },
     },
 
-    -- NOTE: The easiest way to do this is to create a `.lazy.lua` in the repo and define it with something like:
-    --
-    -- ```lua
-    -- return {
-    --   {
-    --     "nvim-neotest/neotest",
-    --     dependencies = {
-    --       "marilari88/neotest-vitest",
-    --     },
-    --     opts = {
-    --       adapters = {
-    --         -- add any custom options here
-    --         ["neotest-vitest"] = {}
-    --       }
-    --     }
-    --   }
-    -- }
-    -- ```
-    --
-    -- or with jest:
-    -- ```lua
-    -- return {
-    --   {
-    --     "nvim-neotest/neotest",
-    --     dependencies = {
-    --       "marilari88/neotest-jest",
-    --     },
-    --     opts = {
-    --       adapters = {
-    --         -- add any custom options here
-    --         ["neotest-jest"] = {}
-    --       }
-    --     }
-    --   }
-    -- }
-    -- ```
-    opts = function(_, opts)
-      if vim.g.current_neotest_framework == "jest" then
-        table.insert(
-          opts.adapters,
-          require("neotest-jest")({
-            -- see section around monorepos
-            cwd = function(file)
-              if string.find(file, "/packages/") then
-                return string.match(file, "(.-/[^/]+/)src")
-              end
-
-              return vim.fn.getcwd()
-            end,
-            jestCommand = function(file)
-              -- when running in the react-md monorepo, I want to use `pnpm test` when running all tests
-              -- since it uses turbo to run
-              if string.match(file, "react%-md$") then
-                return "pnpm test -- --"
-              end
-
-              return require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h"))
-            end,
+    keys = {
+      {
+        "<leader>tu",
+        function()
+          local neotest = require("neotest")
+          neotest.run.run({
+            vim.fn.expand("%:p"),
+            suite = false,
+            extra_args = { "-u" },
           })
-        )
-      end
+        end,
+      },
+    },
+    opts = {
+      adapters = {
+        -- if I need to override stuff, just add a `.lazy.lua` to the repo to add additional config
+        ["neotest-vitest"] = {},
+        ["neotest-jest"] = {
+          -- see section around monorepos
+          cwd = function(file)
+            if string.find(file, "/packages/") then
+              return string.match(file, "(.-/[^/]+/)src")
+            end
 
-      if vim.g.current_neotest_framework == "vitest" then
-        table.insert(opts.adapters, require("neotest-vitest")({}))
-      end
-    end,
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+    },
   },
 }
