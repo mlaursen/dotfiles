73749bb951b568dcfe0dd76a7a8e3dcd1281f42d
Mikkel Laursen <mlaursen03@gmail.com>
2025-05-24 09:27:30 -0400

    chore(nvim): enable copilot just to handle the Copilot auth for chat

--- lazyvim/nvim/lua/plugins/ai.lua
+++ lazyvim/nvim/lua/plugins/ai.lua
@@ -1,5 +1,20 @@
 return {
   {
+    -- minimal copy from `LazyExtra ai.copilot` that just enables the `Copilot
+    -- auth` part. I don't want any of the suggestions
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        hide_during_completion = true,
+      },
+    },
+  },
+  {
     "CopilotChat.nvim",
     opts = {
       -- window = {
