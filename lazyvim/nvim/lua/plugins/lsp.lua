498c63e9c3bfde86b35aef271af908cd8045a184
Mikkel Laursen <mlaursen03@gmail.com>
2025-06-03 17:12:57 -0400

    feat(nvim): remove noise from auto-import suggestions

--- lazyvim/nvim/lua/plugins/lsp.lua
+++ lazyvim/nvim/lua/plugins/lsp.lua
@@ -99,7 +99,22 @@ return {
           settings = {
             typescript = {
               preferences = {
-                autoImportSpecifierExcludeRegexes = { "^@mui/(?!x-)[^/]+$" },
+                autoImportSpecifierExcludeRegexes = {
+                  -- no `import {} from "@mui/material"`, but `import {} from "@mui/x-*"`
+                  "^@mui/(?!x-)[^/]+$",
+
+                  -- i normally want vitest or jest instead
+                  "^node:test$",
+
+                  -- require `node:` for the core modules
+                  "^(child_process|fs|path)$",
+
+                  -- I don't need node internals for 99% of my projects, so remove from auto-imports and suggestions
+                  "^(node:)?(assert|async_hooks|buffer|cluster|console|constants|crypto|dgram|diagnostics_channel|dns|domain|events|http|http2|https|inspector|module|net|os|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|trace_events|tty|url|util|v8|vm|wasi|worker_threads|zlib)",
+
+                  -- I don't need imports from these most of the time
+                  "^(node_modules/|next/dist/|typescript)",
+                },
               },
             },
           },
