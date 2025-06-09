62f927b6e49dc6faba22de608e4becde72fc4bc1
Mikkel Laursen <mlaursen03@gmail.com>
2025-06-09 11:51:10 -0400

    chore(nvim): try using codecompanion since I gotta use AI for work

--- lazyvim/nvim/lua/plugins/ai.lua
+++ lazyvim/nvim/lua/plugins/ai.lua
@@ -1,3 +1,4 @@
+local use_code_companion = true
 return {
   {
     -- minimal copy from `LazyExtra ai.copilot` that just enables the `Copilot
@@ -16,6 +17,7 @@ return {
   },
   {
     "CopilotChat.nvim",
+    enabled = not use_code_companion,
     opts = {
       -- window = {
       --   layout = "float",
@@ -28,5 +30,141 @@ return {
         },
       },
     },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    enabled = use_code_companion,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end,
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
+    },
+    enabled = use_code_companion,
+    keys = {
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      {
+        "<leader>aa",
+        function()
+          require("codecompanion").toggle()
+        end,
+        desc = "Toggle (CodeCompanion Chat)",
+        mode = { "n", "v" },
+      },
+      -- {
+      --   "<leader>aq",
+      --   function()
+      --     vim.ui.input({
+      --       prompt = "Quick Chat: ",
+      --     }, function(input)
+      --       if input ~= "" then
+      --         require("codecompanion").chat({ input })
+      --       end
+      --     end)
+      --   end,
+      --   desc = "Quick Chat (CopilotChat)",
+      --   mode = { "n", "v" },
+      -- },
+      {
+        "<leader>ap",
+        function()
+          require("codecompanion").actions()
+        end,
+        desc = "Run CodeCompanion Tool",
+      },
+      {
+        "q",
+        ft = "codecompanion",
+        desc = "Toggle (CodeCompanion Chat)",
+        remap = true,
+        mode = { "n" },
+      },
+    },
+    config = function()
+      require("codecompanion").setup({
+        adapters = {
+          -- ollama = {
+          --   model = "devstral",
+          --   host = "http://localhost:11434",
+          -- },
+        },
+        -- default_adapter = "ollama",
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              make_vars = true,
+              make_slash_commands = true,
+              show_result_in_chat = true,
+            },
+          },
+        },
+        display = {
+          chat = {
+            window = {
+              opts = {
+                relativenumber = false,
+                number = false,
+              },
+            },
+          },
+        },
+        strategies = {
+          chat = {
+            roles = {
+              user = vim.env.USER or "Me",
+            },
+            keymaps = {
+              close = {
+                modes = {
+                  n = "gq",
+                },
+                index = 4,
+                callback = "keymaps.close",
+                description = "Close Chat",
+              },
+              stop = {
+                modes = {
+                  n = "<c-x>",
+                },
+                index = 5,
+                callback = "keymaps.stop",
+                description = "Stop Request",
+              },
+              clear = {
+                modes = {
+                  n = "gx",
+                },
+                index = 6,
+                callback = "keymaps.clear",
+                description = "Clear Chat",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "codecompanion",
+        title = "CodeCompanion Chat",
+        size = { width = 50 },
+      })
+    end,
   },
 }
