7541eac10f5cf57957fc72f40733bf4c61e3255f
Mikkel Laursen <mlaursen03@gmail.com>
2025-10-03 17:34:39 -0400

    fix(treesitter): use better scss

--- lazyvim/nvim/lua/plugins/treesitter.lua
+++ lazyvim/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,21 @@
 return {
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = function(_, opts)
-  --     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  --
-  --     -- the default [tree-sitter-scss](https://github.com/serenadeai/tree-sitter-scss) is unmaintained
-  --     -- and doesn't support everything that actually matters:
-  --     -- - maps, !default, @use, @forward, variable properties, ...etc
-  --     parser_config.scss.install_info.url = "https://github.com/savetheclocktower/tree-sitter-scss"
-  --
-  --     -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-  --     vim.list_extend(opts.ensure_installed, {
-  --       "scss",
-  --     })
-  --
-  --     opts.highlight.disable = { "scss" }
-  --   end,
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- the default [tree-sitter-scss](https://github.com/serenadeai/tree-sitter-scss) is unmaintained
+      -- and doesn't support everything that actually matters:
+      -- - maps, !default, @use, @forward, variable properties, ...etc
+      require("nvim-treesitter.parsers").scss.install_info = {
+        revision = "HEAD",
+        url = "https://github.com/savetheclocktower/tree-sitter-scss",
+      }
+
+      -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+      vim.list_extend(opts.ensure_installed, {
+        "scss",
+      })
+
+      -- opts.highlight.disable = { "scss" }
+    end,
+  },
 }
