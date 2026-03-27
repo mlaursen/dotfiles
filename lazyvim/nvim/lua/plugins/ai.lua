c0c95d63108d15b6796fb366d75d4f38221cd8e9
Mikkel Laursen <mlaursen03@gmail.com>
2026-03-26 20:27:11 -0400

    fix: disable claude and conditional copilot

--- lazyvim/nvim/lua/plugins/ai.lua
+++ lazyvim/nvim/lua/plugins/ai.lua
@@ -1,12 +1,12 @@
-local use_claude_code = vim.env.ANTHROPIC_API_KEY and vim.env.ANTHROPIC_API_KEY ~= "" and true or false
 local use_copilot_auth = false
+local use_copilot_chat = vim.env.USE_COPILOT == "true" and true or false
 
 return {
   {
     -- minimal copy from `LazyExtra ai.copilot` that just enables the `Copilot
     -- auth` part. I don't want any of the suggestions
     "zbirenbaum/copilot.lua",
-    enabled = not use_claude_code and use_copilot_auth,
+    enabled = use_copilot_auth,
     cmd = "Copilot",
     build = ":Copilot auth",
     event = "BufReadPost",
@@ -21,7 +21,7 @@ return {
   -- NOTE: Must enable CopilotChat extra as well for markup changes
   {
     "CopilotChat.nvim",
-    enabled = not use_claude_code,
+    enabled = use_copilot_chat,
     opts = {
       -- window = {
       --   layout = "float",
@@ -31,22 +31,6 @@ return {
         reset = {
           normal = "<C-r>",
           insert = "<C-r>",
-        },
-      },
-    },
-  },
-
-  {
-    "coder/claudecode.nvim",
-    enabled = use_claude_code,
-    opts = {
-      terminal = {
-        ---@module "snacks"
-        ---@type snacks.win.Config|{}
-        snacks_win_opts = {
-          position = "right",
-          width = 1,
-          -- height = 0.9,
         },
       },
     },
