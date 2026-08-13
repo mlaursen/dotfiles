e1c5a281a94170cde3602228d719c00782336481
Mikkel Laursen <mlaursen03@gmail.com>
2026-08-13 14:40:55 -0400

    chore: remove commented out stable things

--- lazyvim/nvim/lua/plugins/disabled.lua
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -8,14 +8,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
 
-  -- I prefer only using "lukas-reineke/indent-blankline.nvim" since the
-  -- indentation never really matters for me and this plugin makes it even
-  -- noisier with animations. disabling the animation still makes it too
-  -- visible
-  -- { "folke/flash.nvim", enabled = false },
-  -- { "lewis6991/gitsigns.nvim", enabled = false },
-
-  -- seems to be breaking things
-  -- { "nvim-mini/mini.ai", enabled = false },
   -- { "nvim-mini/mini.pairs", enabled = false },
 }
